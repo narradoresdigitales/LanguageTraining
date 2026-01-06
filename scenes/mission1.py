d8723aa970f66ad1fe3e1b6982afb1ae424e6235
sil <narradoresdigitales@gmail.com>
2026-01-06 05:47:55 -0600

    added player input logic to print in terminal

--- scenes/mission1.py
+++ scenes/mission1.py
@@ -1,19 +1,32 @@
 import pygame
-
 from settings import WHITE, BLACK, FONT_NAME, FONT_SIZE
 
 class Mission1Scene:
     def __init__(self, screen):
         self.screen = screen
         self.font = pygame.font.SysFont(FONT_NAME, FONT_SIZE)
+        self.input_text = ''
         
     def handle_event(self, event):
-        pass
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE:
+                self.input_text = self.input_text[:-1]
+            elif event.key == pygame.K_RETURN:
+                print('Player submitted:', self.input_text)
+                self.input_text = '' 
+            else:
+                self.input_text += event.unicode
     
     def update(self):
         pass
     
     def draw(self):
         self.screen.fill(WHITE)
+        
+        #Mission text
         text = self.font.render('Mission 1: Interview in Progress ...', True, BLACK)
         self.screen.blit(text,(50, 50))
+        
+        # Input prompt
+        prompt = self.font.render('Your Response: ' + self.input_text, True, BLACK)
+        self.screen.blit(prompt, (50, 150))
